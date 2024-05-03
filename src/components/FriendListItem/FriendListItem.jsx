ec25bed97e53bb1123b4d28195305f15ecf1edba
Dorian Devua <Dorian.DevUa@gmail.com>
2024-05-03 13:38:26 +0400

    Add formatting from Prettier

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,12 +1,14 @@
 import PropTypes from 'prop-types';
-import css from './FriendListItem.module.css'
+import css from './FriendListItem.module.css';
 
 function FriendListItem({ avatar, name, isOnline }) {
   return (
-    <div className={css.friendCard}>
+    <div className={css.card}>
       <img src={avatar} alt={name} width="48" />
-      <p className={css.friendName}>{name}</p>
-      <p className={`${css.friendStatus} ${isOnline ? css.online : css.offline}`}>{isOnline ? 'Online' : 'Offline '}</p>
+      <p className={css.name}>{name}</p>
+      <p className={`${css.status} ${isOnline ? css.online : css.offline}`}>
+        {isOnline ? 'Online' : 'Offline '}
+      </p>
     </div>
   );
 }
